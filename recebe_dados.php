91fa2a9dcef6c0c4363c652c8c7d3354fe926d3f
Deivid Martins <md031194@gmail.com>
2022-10-20 23:10:22 -0300

    pagina que valida dados das mensagens de ermail

---
+++ recebe_dados.php
@@ -0,0 +1,37 @@
+<?php
+//print_r($_POST);
+
+class mensagem {
+  private $para = null;
+  private $assunto = null;
+  private $mensagem = null;
+  
+  public function __GET($atributo){ //pegar atributos
+    return $this->atributo;
+  }
+
+  public function __SET($atributo,$valor){ //atribuir valores ao atributos.
+    $this->$atributo = $valor;
+
+  }
+  public function mensagemValida(){
+    if (empty($this->para) || empty($this->assunto) || empty($this->mensagem))
+    return false;
+  }
+}
+
+  $mensagem = new Mensagem();
+  $mensagem->__SET('para',$_POST['para']);
+  $mensagem->__SET('assunto',$_POST['assunto']);
+  $mensagem->__SET('mensagem',$_POST['mensagem']);
+
+  if ($mensagem->mensagemValida()){
+    echo 'Mensagem é valida';
+  }
+
+
+
+
+
+
+?>

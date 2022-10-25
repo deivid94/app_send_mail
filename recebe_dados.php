514c102d4837eff2e8db9169eb80db7d45343dc4
Deivid Martins <md031194@gmail.com>
2022-10-25 19:16:10 -0300

    Incluindo a lib PHPMailer

--- recebe_dados.php
+++ recebe_dados.php
@@ -1,5 +1,12 @@
 <?php
-//print_r($_POST);
+ require "./bibliotecas/PHPMailer/Exception.php";
+ require "./bibliotecas/PHPMailer/OAuth.php";
+ require "./bibliotecas/PHPMailer/PHPmailer.php";
+ require "./bibliotecas/PHPMailer/POP3.php";
+ require "./bibliotecas/PHPMailer/SMTP.php";
+
+ use PHPMailer\PHPMailer\PHPMailer;
+ use PHPMailer\PHPMailer\Exception;
 
 class mensagem {
   private $para = null;
@@ -27,10 +34,47 @@ class mensagem {
   $mensagem->__SET('assunto',$_POST['assunto']);
   $mensagem->__SET('mensagem',$_POST['mensagem']);
 
-  if ($mensagem->mensagemValida()){
-    echo 'Mensagem é valida';
-  } else {
+  if (!$mensagem->mensagemValida()){
     echo 'Mensagem nao é valida';
+    die();
   }
+
+  //Instantiation and passing `true` enables exceptions
+$mail = new PHPMailer(true);
+
+try {
+    //Server settings
+    $mail->SMTPDebug = false;                      //Enable verbose debug output
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host       = 'smtp.example.com';                     //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    $mail->Username   = 'user@example.com';                     //SMTP username
+    $mail->Password   = 'secret';                               //SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         //Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+    $mail->Port       = 587;                                    //TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+
+    //Recipients
+    $mail->setFrom('from@example.com', 'Mailer');
+    $mail->addAddress('joe@example.net', 'Joe User');     //Add a recipient
+    $mail->addAddress('ellen@example.com');               //Name is optional
+    $mail->addReplyTo('info@example.com', 'Information');
+    $mail->addCC('cc@example.com');
+    $mail->addBCC('bcc@example.com');
+
+    //Attachments
+    $mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
+    $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
+
+    //Content
+    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->Subject = 'Here is the subject';
+    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+    $mail->send();
+    echo 'Nao foi possivel enviar este email tente novamente';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
 
 ?>
